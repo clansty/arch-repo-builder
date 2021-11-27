d5c462c66ab7fdca3d30445631d866c239121a99
凌莞 <i@gao4.pw>
2021-11-27 12:40:15 +0800

    做成异步的

--- repo-builder.js
+++ repo-builder.js
@@ -1,6 +1,9 @@
-const fs = require('fs')
-const {execSync} = require('child_process')
+const fs = require('fs/promises')
+const fsO = require('fs')
 const path = require('path')
+const util = require('util');
+
+const exec = util.promisify(require('child_process').exec)
 
 const TMP_PACKAGE_STORE = '/work/tmp'
 const REPO_PATH = '/work/repo'
@@ -12,7 +15,7 @@ const waitingQueue = []
 /**
  * @param {string} pkgFileName
  */
-const addPackage = (pkgFileName) => {
+const addPackage = async (pkgFileName) => {
     if (workingLock) {
         waitingQueue.push(pkgFileName)
         return
@@ -22,8 +25,8 @@ const addPackage = (pkgFileName) => {
     try {
         // 从文件名中获取架构名称
         //            [----1名称版本----]-[---2架构---] .pkg .tar .zst/.gz/.xz?
-        const exec = /^([A-Za-z0-9\-._]+)-([a-z0-9._]+)\.pkg\.tar(\.[a-z0-9]+)?$/.exec(pkgFileName)
-        const archName = exec[2]
+        const regexExec = /^([A-Za-z0-9\-._]+)-([a-z0-9._]+)\.pkg\.tar(\.[a-z0-9]+)?$/.exec(pkgFileName)
+        const archName = regexExec[2]
 
         let arches = [archName]
         if (archName === 'any') {
@@ -33,26 +36,26 @@ const addPackage = (pkgFileName) => {
         for (const arch of arches) {
             // 创建架构目录，如果不存在
             const archPath = path.join(REPO_PATH, arch)
-            if (!fs.existsSync(archPath)) {
-                fs.mkdirSync(archPath)
+            if (!fsO.existsSync(archPath)) {
+                await fs.mkdir(archPath)
             }
 
             // 执行添加命令
-            if (fs.existsSync(path.join(archPath, `${REPO_NAME}.db.tar.gz.lck`)))
-                fs.unlinkSync(path.join(archPath, `${REPO_NAME}.db.tar.gz.lck`))
-            execSync(`repo-add -R ${path.join(archPath, `${REPO_NAME}.db.tar.gz`)} ${path.join(TMP_PACKAGE_STORE, pkgFileName)}`)
+            if (fsO.existsSync(path.join(archPath, `${REPO_NAME}.db.tar.gz.lck`)))
+                await fs.unlink(path.join(archPath, `${REPO_NAME}.db.tar.gz.lck`))
+            await exec(`repo-add -R ${path.join(archPath, `${REPO_NAME}.db.tar.gz`)} ${path.join(TMP_PACKAGE_STORE, pkgFileName)}`)
         }
 
         if (archName === 'any') {
             for (const arch of arches) {
-                fs.copyFileSync(path.join(TMP_PACKAGE_STORE, pkgFileName), path.join(REPO_PATH, arch, pkgFileName))
+                await fs.copyFile(path.join(TMP_PACKAGE_STORE, pkgFileName), path.join(REPO_PATH, arch, pkgFileName))
             }
-            fs.unlinkSync(path.join(TMP_PACKAGE_STORE, pkgFileName))
+            await fs.unlink(path.join(TMP_PACKAGE_STORE, pkgFileName))
         }
         else {
             // 将文件移动到架构目录
             const dest = path.join(REPO_PATH, archName, pkgFileName)
-            fs.renameSync(path.join(TMP_PACKAGE_STORE, pkgFileName), dest)
+            await fs.rename(path.join(TMP_PACKAGE_STORE, pkgFileName), dest)
         }
     }
     catch (e) {
@@ -61,7 +64,7 @@ const addPackage = (pkgFileName) => {
     finally {
         workingLock = false
         if (waitingQueue.length > 0) {
-            addPackage(waitingQueue.shift())
+            await addPackage(waitingQueue.shift())
         }
     }
 }
